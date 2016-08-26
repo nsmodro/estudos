dd28db78921b398f09ba28a3645eb0367892c100
nsmodro <natacha.modro@gmail.com>
2016-08-26 13:36:25 -0300

    if js

--- curso-js/projeto/calcula-imc.js
+++ curso-js/projeto/calcula-imc.js
@@ -1,8 +1,11 @@
 //imc = peso/(altura*altura)
 
-var peso = 57
-var altura = 1.7
+var peso = 59;
+var altura = 1.7;
 
-var imc = peso/(altura*altura)
+var alturaDiferenteDeZero = (altura > 0); //true or false
 
-console.log(imc);
+if (alturaDiferenteDeZero) {
+	var imc = peso/(altura*altura);
+	console.log(imc);
+} else console.log("altura tem que ser positiva e diferente de zero");
