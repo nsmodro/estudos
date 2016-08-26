f607facb83dd37d5690ae56eda11e92ef085e9b2
nsmodro <natacha.modro@gmail.com>
2016-08-26 17:19:06 -0300

    Colecoes e lacos

--- curso-js/projeto/calcula-imc.js
+++ curso-js/projeto/calcula-imc.js
@@ -1,24 +1,56 @@
 //imc = peso/(altura*altura)
 
+var trsPacientes = document.getElementsByClassName("paciente"); //array de trs
+
+
+for (var i = 0; i < trsPacientes.length; i++){
+	var trPaciente = trsPacientes[i];
+	var tdNome = trPaciente.getElementsByClassName("info-nome")[0];
+	var tdPeso = trPaciente.getElementsByClassName("info-peso")[0];
+	var tdAltura = trPaciente.getElementsByClassName("info-altura")[0];
+
+	var paciente = {nome : tdNome.textContent, peso : tdPeso.textContent, altura: tdAltura.textContent};
+
+	if (paciente.altura > 0) {
+		var imc = paciente.peso/(paciente.altura*paciente.altura);
+		var tdImc = trPaciente.getElementsByClassName("info-imc")[0];
+		tdImc.textContent = imc;
+		
+	} else console.log("altura tem que ser positiva e diferente de zero");
+}
+
+
+/* **************** while
 var tdPeso = document.getElementById("peso-1");
 var tdAltura = document.getElementById("altura-1");
 
 //Obj em js
-var paciente = {"peso" : tdPeso.textContent, "altura" : tdAltura.textContent};
+var paciente1 = {"peso" : tdPeso.textContent, "altura" : tdAltura.textContent};
 
-var alturaDiferenteDeZero = (paciente.altura > 0); //true or false
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
+var paciente2 = {"peso" : tdPeso.textContent, "altura" : tdAltura.textContent};
 
 
-if (alturaDiferenteDeZero) {
-	var imc = paciente.peso/(paciente.altura*paciente.altura);
+var pacientes = [paciente1, paciente2];
 
-	tdImc = document.getElementById("imc-1");
-	tdImc.textContent = imc;
+var i = 0;
+while (i < pacientes.length){
+	var paciente = pacientes[i];
 
-	console.log(imc);
-} else console.log("altura tem que ser positiva e diferente de zero");
+	if (paciente.altura > 0) {
+		var imc = paciente.peso/(paciente.altura*paciente.altura);
+		console.log(imc);
+		tdImc = document.getElementById("imc-1");
+		tdImc.textContent = imc;
+	} else console.log("altura tem que ser positiva e diferente de zero");
 
-/*
+	i++;
+}
+*/
+
+
+/* *********** modo simples if
 var peso = 59;
 var altura = 1.7;
 
